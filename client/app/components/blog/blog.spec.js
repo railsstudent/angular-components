f1ef624d06a469383926e780dd9faa1774860a92
Connie Leung <cleung334@gmail.com>
2017-05-09 04:24:36 +0800

    Exercise 4's exercise do not work.

--- client/app/components/blog/blog.spec.js
+++ client/app/components/blog/blog.spec.js
@@ -7,7 +7,9 @@ describe('Blog', () => {
   let $rootScope;
   let makeController;
 
-  beforeEach(window.module(blog.name));
+  console.log(template);
+
+  //beforeEach(window.module(blog.name));
   beforeEach(inject(_$rootScope_ => {
     $rootScope = _$rootScope_;
     makeController = (injectables) => {
@@ -24,14 +26,31 @@ describe('Blog', () => {
   describe('directive', ()=> {
     // TODO: test the directive to make sure it has the
     // right DDO and template and controller
+    let ddo;
+    beforeEach(() => {
+        ddo = blogDirective();
+    });
+    
+    it ('should have the right template, controller', () => {
+        expect(ddo.controller).to.equal(BlogController);
+        expect(ddo.template).to.equal(template);
+        expect(ddo.scope).to.be.an('object');
+        expect(ddo.controllerAs).to.be.a('string');
+    });
   });
 
   describe('controller', ()=> {
     // TODO: test the controller
+    it('should have an appropriate controller', () => {
+      const controller = makeController();
+      expect(controller.posts).to.be.an('array');
+      expect(controller.message).to.be.a('string');
+    });
   });
 
   describe('template', ()=> {
     // TODO: test the template? yup
     // get creative
+    console.log(template);
   });
 });
